d2742f7082216f52033b3507f120e419329007d3
Avinash <avinash@linearsix.com>
2024-02-09 16:48:07 +0530

    Stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import "./globals.css";
 import { Routes, Route } from "react-router-dom";
 import SigninForm from "./auth/forms/SigninForm";
-import { Home } from "./root/pages";
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from "./root/pages";
 import SignupForm from "./auth/forms/SignupForm";
 import AuthLayout from "./auth/AuthLayout";
 import RootLayout from "./root/RootLayout";
-import { Toaster } from "@/components/ui/toaster"
-
+import { Toaster } from "@/components/ui/toaster";
 
 const App = () => {
   return (
@@ -22,6 +21,14 @@ const App = () => {
         {/* private*/}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
       <Toaster />
