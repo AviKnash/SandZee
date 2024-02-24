42f3d4a5eec7622b68d189b499b04b3159de29bf
Avinash <avinash@linearsix.com>
2024-02-24 21:30:01 +0530

    Final

--- src/components/shared/TopBar.tsx
+++ src/components/shared/TopBar.tsx
@@ -21,7 +21,7 @@ const TopBar = () => {
       <div className="flex-between py-4 px-5">
         <Link to="/" className="flex gap-3 items-center">
           <img
-            src="/assets/images/logo.svg"
+            src="/assets/images/bubble.png"
             alt="logo"
             width={130}
             height={325}
