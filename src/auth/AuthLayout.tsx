4c106b741555235cca8b422a2a27a119369ea25b
AvinashINS <avinash.a@insighture.com>
2024-10-25 21:48:11 +0530

    Changed img ui

--- src/auth/AuthLayout.tsx
+++ src/auth/AuthLayout.tsx
@@ -15,7 +15,7 @@ const AuthLayout = () => {
           </section>
 
           <img
-            src="/assets/images/media.svg"
+            src="/assets/images/media.png"
             alt="logo"
             className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
           />
