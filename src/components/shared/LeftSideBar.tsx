093f9b8caccb1a8dbcb6a2a879ff6a2314d22ab8
AvinashINS <avinash.a@insighture.com>
2024-10-25 21:51:10 +0530

    Changed use efect for side bar

--- src/components/shared/LeftSideBar.tsx
+++ src/components/shared/LeftSideBar.tsx
@@ -13,11 +13,7 @@ const LeftSideBar = () => {
 
   const { user } = useUserContext();
 
-  useEffect(() => {
-    if (isSuccess) {
-      navigate(0);
-    }
-  }, [isSuccess]);
+
   return (
     <nav className="leftsidebar">
       <div className="flex flex-col gap-11">
