a87bb183836c0e7f93d4031ecca895fb27c77aa4
AvinashINS <avinash.a@insighture.com>
2024-10-25 21:43:11 +0530

    Test

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -87,7 +87,7 @@ const PostForm = ({ post, action }: PostFormProps) => {
           name="caption"
           render={({ field }) => (
             <FormItem>
-              <FormLabel className="shad-form_label">Caption 2</FormLabel>
+              <FormLabel className="shad-form_label">Caption</FormLabel>
               <FormControl>
                 <Textarea
                   className="shad-textarea custom-scrollbar"
