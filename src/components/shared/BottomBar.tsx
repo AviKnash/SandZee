509af8ffd39d16d9eb9bc65bf3b2963678efa7d8
AvinashINS <avinash.a@insighture.com>
2024-10-25 21:49:02 +0530

    Removed botton bar img to test

--- src/components/shared/BottomBar.tsx
+++ src/components/shared/BottomBar.tsx
@@ -16,13 +16,7 @@ const BottomBar = () => {
             } flex-center flex-col gap-1 p-2 transition`}
             to={link.route}
           >
-            <img
-              src={link.imgURL}
-              alt={link.label}
-              width={16}
-              height={16}
-              className={`${isActive && "invert-white"}`}
-            />
+
             <p className="tiny-medium text-light-2">{link.label}</p>
           </Link>
         );
