3e516bb90f5df690a5a7e90ce727f975cb017c0a
AvinashINS <avinash.a@insighture.com>
2024-10-25 21:45:41 +0530

    Readjusted ui

--- src/auth/forms/SigninForm.tsx
+++ src/auth/forms/SigninForm.tsx
@@ -78,7 +78,9 @@ const SigninForm = () => {
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
         <img src="/assets/images/bubble-auth.png" draggable="false" alt="logo" />
-   
+        <h2 className="hr-bold md:h2-bold pt-5 sm:pt-12">
+          Login to your account
+        </h2>
 
         <p className="text-light-3 small-medium md:base-regular mt-2">
           Welcome back, please enter your details
